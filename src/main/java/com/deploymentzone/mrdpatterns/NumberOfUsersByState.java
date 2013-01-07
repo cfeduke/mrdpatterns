46eb1358c2c66a4afcc3ad1320ef0a181a438443
Charles Feduke <charles.feduke@gmail.com>
2013-01-07 13:25:31 -0500

    removed unused import

--- src/main/java/com/deploymentzone/mrdpatterns/NumberOfUsersByState.java
+++ src/main/java/com/deploymentzone/mrdpatterns/NumberOfUsersByState.java
@@ -13,7 +13,6 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Counter;
